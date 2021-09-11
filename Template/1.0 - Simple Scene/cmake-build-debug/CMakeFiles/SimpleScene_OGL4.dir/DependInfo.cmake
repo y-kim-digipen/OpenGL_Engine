9803512b50663073dc3ff005733eb76ff4690448
Yoonki Kim <y.kim@digipen.edu>
2021-09-10 17:31:44 -0700

    Initial setup from CS300 Code template

---
+++ Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/SimpleScene_OGL4.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/GLApplication.cpp" "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/SimpleScene_OGL4.dir/GLApplication.cpp.o"
+  "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/SimpleScene_Quad.cpp" "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/SimpleScene_OGL4.dir/SimpleScene_Quad.cpp.o"
+  "/home/yoonki/Desktop/CS300/Template/Common/shader.cpp" "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/SimpleScene_OGL4.dir/home/yoonki/Desktop/CS300/Template/Common/shader.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../Common"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yoonki/Desktop/CS300/Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/common_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Template/1.0 - Simple Scene/cmake-build-debug/CMakeFiles/SimpleScene_OGL4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleScene_OGL4.dir/GLApplication.cpp.o"
+  "CMakeFiles/SimpleScene_OGL4.dir/SimpleScene_Quad.cpp.o"
+  "CMakeFiles/SimpleScene_OGL4.dir/home/yoonki/Desktop/CS300/Template/Common/shader.cpp.o"
+  "SimpleScene_OGL4"
+  "SimpleScene_OGL4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleScene_OGL4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
